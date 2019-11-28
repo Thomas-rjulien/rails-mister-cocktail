0e52387131a496abeb76ec141356fa8cfdd80a7a
thomas julien <thomas.julien@skema.edu>
2019-11-28 17:20:44 +0100

    End of mister Cocktail with no style

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :cocktails, except: %i[destroy edit update] do
+    resources :doses, only: %i[new create destroy]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
